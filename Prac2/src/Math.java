0300ba02d9a042a7a54e139fd0fbbcd1e88df258
Janisha <Janisha@LAPTOP-UJEJ5S9C>
2022-04-26 22:58:59 +0530

    Commit2

---
+++ Prac2/src/Math.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Math{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	
+		
+		System.out.println("Enter any number");
+		Scanner reader = new Scanner(System.in); 
+		int num = reader.nextInt();
+		if((num%2!=0)&&(num>=100))
+		{
+		System.out.println("Success!!!");
+		}
+		else 
+			System.out.println("Failure");
+		reader.close();
+	}
+
+}
